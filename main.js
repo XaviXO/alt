13b1b4fc8cb5cd8a296cea7cbe236131a806e119
Javier Calderon <javierjcjr@gmail.com>
2020-03-28 13:16:55 -0700

    scroll animation

--- main.js
+++ main.js
@@ -8,4 +8,21 @@ let body = selectElement('body');
 
 menuToggler.addEventListener('click', function(){
     body.classList.toggle('open')
-})
+});
+
+// scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin: 'left',
+    duration: 1000,
+    distace:'25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right',{
+    origin: 'right',
+    duration: 1000,
+    distace:'25rem',
+    delay: 600
+});

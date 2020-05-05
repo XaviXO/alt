3a8f8e897a058b86a1d6917749a23609259a5f5e
Javier Calderon <javierjcjr@gmail.com>
2020-05-05 14:27:15 -0700

    updated code

--- gallery/main.js
+++ gallery/main.js
@@ -40,13 +40,3 @@ sr.reveal('.animate-bottom',{
     distace:'25rem',
     delay: 600
 });
-
-function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 37.47544705200545, lng: -122.21786401785971};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-}
